5fc7479ca2d3ecf60f03eb806eb557504fcbe9e3
M-Thanu <mahanthanuja@gmail.com>
2024-04-24 17:05:25 +0530

    escape sequence & format specifiers

---
+++ escape.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    printf("Let's try\t");
+    printf("See what's happen\\");
+    printf("That's it");
+    
+    int myNum = 15;
+    float decNum = 5.99;
+    printf("%f\n", decNum);
+
+    char lettr = 'D';
+    printf("%c\n", lettr);
+    printf("%d", lettr);
+
+    printf("My Lucky letter is %c and my Lucky number is %d", lettr, myNum);
+
+    return 0;
+}
