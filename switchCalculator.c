f4ec918025bceac322cc70e0895c95f53848c3f6
M-Thanu <mahanthanuja@gmail.com>
2024-05-29 22:34:48 +0530

    building

---
+++ switchCalculator.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+int main(){
+    printf("Welcome to the Calculator");
+    int a;
+    int b;
+    int op;
+    printf("Please input your numbers");
+    printf("Enter the first number");
+    scanf("%d",&a);
+    printf("Enter the second number");
+    scanf("%d",&b);
+    printf("Operations available are : \n 1 - Addition\n2-Subtraction\n3-Multiplication\n4-Division\nPlease select your number: ");
+    scanf("%d", &op);
+
+    switch(op){
+        
+    }
+}
