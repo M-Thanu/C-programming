5c8792df84f730042416975ab1af04248a261ca0
M-Thanu <mahanthanuja@gmail.com>
2024-06-03 10:48:25 +0530

    computing total course fees

--- ex2q2.c
+++ ex2q2.c
@@ -5,8 +5,8 @@ int main(){
     char name[50];
     char id[30];
     int dst;
-    int af;
-    int tf;
+    float af;
+    float tf=200.00;
 
     printf("Enter your name : ");
     scanf("%s",&name);
@@ -17,19 +17,17 @@ int main(){
     printf("Enter your duration of study : ");
     scanf("%d",&dst);
 
-    /*printf("Name : %s \n ID : %s \n Duration of Study : %d \n",name,id,dst);
+    printf("Name : %s \n ID : %s \n Duration of Study : %d \n",name,id,dst);
     printf("Annual Fee :\n");
     printf("Year 1: $200\n");
 
     for (int i=1; i<dst; i++){
-        af = [200+{200 * (5/100) * i}];
-        printf("Year %d: $%d\n", i+1,af);
+        af = 200.00+200.00 * (5.00/100.00) * i;
+        printf("Year %d: $%.2f\n", i+1,af);
         tf = tf + af;
     }
 
-    printf("Total Fee: %d",tf);*/
-
-    printf("%d", (200*(5/100)));
+    printf("Total Fee: $%.2f",tf);
 
     return 0;
 }
