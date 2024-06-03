b8084a6b637722fa54655a47bf3f83674f7854dc
M-Thanu <mahanthanuja@gmail.com>
2024-06-03 11:00:46 +0530

    discount calculator

--- ex2q3.c
+++ ex2q3.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main(){
+    float puramt;
+    float payamt;
 
+    printf("Enter purchase amount: ");
+    scanf("%f",&puramt);
+
+    if (puramt>5000.00){
+        payamt = puramt - (15.00/100.00)*puramt;
+    }
+    else{
+        payamt = puramt - (10.00/100.00)*puramt;
+    }
+
+    printf("Your amount: %.2f",payamt);
+    
     return 0;
 }
