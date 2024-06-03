d628df84f1b24bc2950c6b75a1ac9fd745b01014
M-Thanu <mahanthanuja@gmail.com>
2024-06-03 20:23:47 +0530

    building up

---
+++ ex2q5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(){
+    int year;
+    float agpa;
+    float tgpa;
+
+    printf("Enter the number of academic years: ");
+    scanf("%d",&year);
+
+    for (int i=1; i<year+1; i++){
+        printf("Enter the GPA of year %d : ",i);
+        scanf("%f",&agpa);
+        tgpa = tgpa + agpa;
+    }
+
+    float dgpa = tgpa/year;
+
+    displayClass(dgpa);
+
+    return 0;
+}
+
+void displayClass(float gpa){
+    if (gpa >= 3.70){
+        printf("First Class");
+    }
+    else if (gpa >= 3.30 && gpa <= 3.70){
+        printf("Second Upper");
+    }
+    else if (gpa >= 3.00 && gpa <= 3.30){
+        printf("Second Lower");
+    }
+    else{
+        printf("Some error");
+    }
+}
