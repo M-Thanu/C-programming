03e6449827d8cd81687817e60d1a4e1453074bb7
M-Thanu <mahanthanuja@gmail.com>
2024-06-06 00:59:20 +0530

    completed

--- ex2q9.c
+++ ex2q9.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 
 int main(){
-    char word[20];
+    char word[100];
+    int l=0;
 
     printf("Enter the word: ");
     scanf("%s", &word);
 
-    /*char a = 'a';
-    char e = 'e';
-    char i = 'i';
-    char o = 'o';
-    char u = 'u';*/
-
+    while (word[l] != '\0'){
+        l++;
+    }
+    
     int ca=0;
     int ce=0;
     int ci=0;
     int co=0;
     int cu=0;
-
-    int l = sizeof(word);
 
     for (int i=0; i<l; i++){
         switch(word[i]){
@@ -38,24 +35,9 @@ int main(){
                 cu=cu+1;
                 break;
         }
-        /*if (word[i] == 'a'){
-            ca+=1;
-        }
-        else if (word[i] == 'e'){
-            ce+=1;
-        }
-        else if (word[i] == 'i'){
-            ci+=1;
-        }
-        else if (word[i] == 'o'){
-            co+=1;
-        }
-        else if (word[i] == 'u'){
-            cu+=1;
-        }*/
     }
 
-    printf("The number of a in the word : %d\n",ca);
+   printf("The number of a in the word : %d\n",ca);
     printf("The number of e in the word : %d\n",ce);
     printf("The number of i in the word : %d\n",ci);
     printf("The number of o in the word : %d\n",co);
