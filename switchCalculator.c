a875a5968dc075d11eb0d3e1ff8aafca25997ce2
M-Thanu <mahanthanuja@gmail.com>
2024-06-07 23:08:50 +0530

    testing

--- switchCalculator.c
+++ switchCalculator.c
@@ -1,14 +1,13 @@
 # include <stdio.h>
 
 int main(){
-    printf("Welcome to the Calculator\n");
     int a;
     int b;
     int op;
     printf("Please input your numbers\n");
-    printf("Enter the first number");
+    printf("Enter the first number : ");
     scanf("%d",&a);
-    printf("Enter the second number");
+    printf("Enter the second number : ");
     scanf("%d",&b);
     printf("Operations available are : \n1 - Addition\n2 - Subtraction\n3 - Multiplication\n4 - Division\nPlease select your number: ");
     scanf("%d", &op);
