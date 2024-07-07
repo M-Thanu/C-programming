59fe11f88c33a7fc466f144dfec140bf662e486f
M-Thanu <mahanthanuja@gmail.com>
2024-07-07 18:54:30 +0530

    Calculating

---
+++ pr5q2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    //Variable definition and value assignment
+    int h=10;
+    int w=12;
+    int d=8;
+
+    //Calculation
+    int V = h*w*d;
+
+    //Output
+    printf("The volume of the cube is : %d",V);
+
+    return 0;
+}
