ea6532b568d0802c6126ecb9a2501a80ee96ff6f
M-Thanu <mahanthanuja@gmail.com>
2024-08-22 17:03:13 +0530

    ex2h

---
+++ Printf/ex2h.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+printf("Printing numbers with and without the + flag.\n");
+printf(" Compare the output with the source code\n");
+printf(" \n\n");
+printf("%d\n%d\n", 786, -786);
+printf("%+d\n%+d\n", 786, -786);
+return 0;
+}
