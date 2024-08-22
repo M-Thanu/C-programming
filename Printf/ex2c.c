bf93c947645deeeaca46a0669732eb64462c7352
M-Thanu <mahanthanuja@gmail.com>
2024-08-22 16:09:00 +0530

    ex2partc

---
+++ Printf/ex2c.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+{
+printf( "<%2s>\n", "Paul must die!" );
+printf( "<%22s>\n", "Paul must die!" );
+printf( "<%22.5s>\n", "Paul must die!" );
+printf( "<%-22.5s>\n", "Paul must die!" );
+}
