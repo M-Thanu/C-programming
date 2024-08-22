0ba71dca2488dcf935b5c8f3b12c3fe98de6546c
M-Thanu <mahanthanuja@gmail.com>
2024-08-22 16:37:50 +0530

    ex2f.c

---
+++ Printf/ex2e.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+printf(" Printing integers right-justified.\n");
+printf("Compare the output with the source code\n");
+printf(" \n\n");
+printf("%4d\n", 1);
+printf("%4d\n", 12);
+printf("%4d\n", 123);
+printf("%4d\n", 1234);
+printf("%4d\n\n", 12345);
+printf("%4d\n", -1);
+printf("%4d\n", -12);
+printf("%4d\n", -123);
+printf("%4d\n", -1234);
+printf("%4d\n", -12345);
+
+
+return 0;
+}
