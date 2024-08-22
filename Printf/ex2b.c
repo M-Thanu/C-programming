f7614c628aa246b6ed2381c78198562021376e7c
M-Thanu <mahanthanuja@gmail.com>
2024-08-22 12:31:00 +0530

    ex2partb

---
+++ Printf/ex2b.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main(){
+    printf( "<%f>\n", 1234.56 );
+    printf( "<%e>\n", 1234.56 );
+    printf( "<%4.2f>\n", 1234.56 );
+    printf( "<%3.1f>\n", 1234.56 );
+    printf( "<%10.3f>\n", 1234.56 );
+    printf( "<%10.3e>\n", 1234.56 );
+}
