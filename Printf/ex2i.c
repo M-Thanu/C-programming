373620a0fe3cd972193d0ba05dc1f60988375595
M-Thanu <mahanthanuja@gmail.com>
2024-08-22 17:06:57 +0530

    ex2i

---
+++ Printf/ex2i.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+printf("Printing a space before signed values\n");
+printf(" not preceded by + or -n\n");
+printf(" \n\n");
+printf("% d\n% d\n", 877, -877);
+return 0;
+}
