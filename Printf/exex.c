bafcd71f1f20cac0ea4e50128d458a2ef7149f8c
M-Thanu <mahanthanuja@gmail.com>
2024-08-22 11:56:15 +0530

    format specifiers

---
+++ Printf/exex.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    printf("%d\n",2+3); 
+    printf("\t%c\n",'A');  
+    printf("\t%c\t%c\n",'A','B'); 
+    printf("%s\n","ABCDE");  
+    printf("%s%s\n","ABC","DEF"); 
+    printf("%f\n",2.3+3.2);
+    printf("\t%c\n",'AB');
+    printf("%c\n",'ABCDE');
+    printf("%s\n","ABC","DEF");
+    printf("%c%c%c%c%d\n",'2','+','3','=',2+3);
+
+    return 0;
+}
---
+++ Printf/var.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    
+        char x, y ;
+        int z ;
+        x = 'a' ;
+        y = 'b' ;
+        z = x + y ;
+
+        printf("%c\n",z);
+        //printf("%d",z);
+
+    
+
+    return 0;
+}
