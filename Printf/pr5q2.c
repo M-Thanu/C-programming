cb57e4a7ea296f9beb77b6346fc61fe46033b79d
M-Thanu <mahanthanuja@gmail.com>
2024-08-23 19:23:22 +0530

    reupdate

--- Printf/pr5q2.c
+++ Printf/pr5q2.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
 int main(){
-    int r,d,s,a;
-    //const float PI=3.145;
+    int r=14;
     #define PI 3.145
-    printf("Enter the radius:");
-    scanf("%d",&r);
     printf("\nDiameter:%d", 2*r);
     printf("\nCircumference:%.2f",2*PI*r);
     printf("\nArea:%.2f",PI*r*r);
