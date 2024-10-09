9b8cdb78fe8ad9f7b61c8a680313b19503dfbf02
M-Thanu <mahanthanuja@gmail.com>
2024-10-10 02:02:33 +0530

    practice

--- ch.c
+++ ch.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hello\b\n");
+    char s='G';
+
+    switch(s){
+        case 'R':
+            printf("D");
+        case 'G':
+            printf("S");
+        case 'B':
+            printf("K");
+        default:
+            printf("H");
+        break;
+    }
     printf("Hi");
     return 0;
 }
