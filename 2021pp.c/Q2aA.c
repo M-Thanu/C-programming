11bd4d87a80953d8efe33bc9ae34626d94e016c3
M-Thanu <mahanthanuja@gmail.com>
2024-10-26 23:30:32 +0530

    revision

--- 2021pp.c/Q2aA.c
+++ 2021pp.c/Q2aA.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 int main(){
-    int fd=10000;
-    int inrest;
-    inrest = (20*fd)/100;
+    float fd=10000.00;
+    float inrest;
     int yc=0;
+    /*
+    inrest = (20*fd)/100;
+    
     yc=8000/inrest;
-    printf("Number of years: %d",yc);
+    
+    */
+   while(inrest<8000.00){
+    inrest = inrest + (0.2*10000);
+    yc=yc+1;
+   }
+   printf("Number of years: %d",yc);
     return 0;
 }
--- 2021pp.c/Q2b.c
+++ 2021pp.c/Q2b.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(){
-    int fd=10000;
-    int inrest;
+    float fd=10000.00;
+    float inrest;
+    /*
     inrest = (1.5*fd*12)/100;
     int t=0;
     t=fd+inrest;
@@ -10,5 +11,12 @@ int main(){
         printf("Total balance at %d year : %d\n",i,t);
         t=t+inrest;
     }
+    */
+   for (int j=1; j<=120; j++){
+    inrest = (1.5*fd)/100;
+    fd=fd+inrest;
+    printf("Total : %.2f\n",fd);
+   }
+
     return 0;
 }
